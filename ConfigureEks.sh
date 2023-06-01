f192da757a58ac7c434ac41cefb5c70e8f778dbf
Jorge Flores <jorge.floresrackspace.com@C02GV0L9Q05P.local>
2023-05-31 19:16:59 -0600

    Add update 63 buildspec

--- ConfigureEks.sh
+++ ConfigureEks.sh
@@ -50,10 +50,11 @@ ServiceAccountAWSALbController=$(kubectl get serviceaccounts aws-load-balancer-c
 if [ "${ServiceAccountAWSALbController}" == "" ]
 then
   eksctl create iamserviceaccount \
-    --cluster ${AMX_PPL_CLUSTER_EKS} \
-    --namespace kube-system \
-    --name aws-load-balancer-controller \
-    --attach-policy-arn arn:aws:iam::${ACCOUNT_ID}:policy/AWSLoadBalancerControllerIAMPolicy \
+    --cluster=${AMX_PPL_CLUSTER_EKS} \
+    --namespace=kube-system \
+    --name=aws-load-balancer-controller \
+    --role-name AmazonEKSLoadBalancerControllerRole \
+    --attach-policy-arn=arn:aws:iam::${ACCOUNT_ID}:policy/AWSLoadBalancerControllerIAMPolicy \
     --override-existing-serviceaccounts \
     --region ${AWS_REGION} --approve
 fi
